703f99657a44192b8cd23098f3c446492b65b4ee
LUCAS FERRAZ CHERUTTI <13106869@l1727031.portoalegre.pucrsnet.br>
2015-11-03 21:05:51 -0200

    Arquivo main

--- catraca.cc
+++ catraca.cc
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+	fstream input;
+	cout << "Catraca" << endl;
+	
+	input.open("cartoes.txt");//ler o "cartao" com id e data de validade
 
-	cout <<"Catraca" << endl;
+	int w/*id*/, x/*ano*/, y/*mes*/, z/*dia*/;
+
+	input >> w >> x >> y >> z;//bota os valores do "cartao" nas variaveis
+
+	cout << "ID: "<< w << endl;//mostra o id
+
+
+	if( x < 2015 ) //averiguar validade
+	{
+		cout << "Error" << endl;
+		return 1;
+	}else
+	{
+		cout << "OK! Passe..." << endl;
+	}
+	input.close();
+
+	//data e hora de entrada do id tal
+	//ler cartao
+	//data e hora da saida do id tal
+
+	return 0;
 }
