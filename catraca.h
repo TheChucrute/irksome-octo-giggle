07d5da953a62a53a1236123904395145396619a7
LEANDRO SANTOS GRASSI CARDOSO <11111917@l1607100.portoalegre.pucrsnet.br>
2015-11-05 20:10:43 -0200

    atualização catraca.h

--- catraca.h
+++ catraca.h
@@ -5,12 +5,15 @@ using namespace std;
 class catraca{
 
 private:
-
-
-
-
+int ID;
+int dia;
+int mes;
+int ano;
+bool situacao;
 public:
-
-
+Cartao (int i, int d, int m, int a);
+void validacao();
+void entrada();
+void saida();
 
 }
