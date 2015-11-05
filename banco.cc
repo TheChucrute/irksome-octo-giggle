24a925a04ed4a399f8053e4909a5eb0e0cd1c1bc
OCTAVIO PITREZ CARPES <13202108@l1607563.portoalegre.pucrsnet.br>
2015-11-05 21:03:23 -0200

    banco

---
+++ banco.cc
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+
+using namespace std;
+
+	class banco{
+
+		public: 
+		
+		bool entrada();
+		bool saida();
+		private:
+
+
+
+bool entrada::banco(){
+
+	
+	
+		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
