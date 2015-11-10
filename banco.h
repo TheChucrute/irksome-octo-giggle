180a189979c690504f000366c4b625917a9c4961
OCTAVIO PITREZ CARPES <13202108@l1818087.portoalegre.pucrsnet.br>
2015-11-10 20:14:25 -0200

    aaa

---
+++ banco.h
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+	class banco{
+
+		public: 
+		
+		void ler();
+		bool entrada();
+
+		private:
+		
+
+	}
+
+int main(){
+
+
+void ler::banco(){
+
+	
+
+}
+
+bool entrada::banco{
+
+}
+}
+
+
