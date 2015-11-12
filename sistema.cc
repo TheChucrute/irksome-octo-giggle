d830dacd58322b0573faaeedbf775af0480bad01
LUCAS FERRAZ CHERUTTI <13106869@l1815125.portoalegre.pucrsnet.br>
2015-11-12 19:56:28 -0200

    sistema v1

---
+++ sistema.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include "cartao.h"
+#include "banco.h"
+
+
+using namespace std;
+
+
+class Sistema {
+
+private:
+	void verificar();
+	Sistema mostrarHora();
+
+};
+
+void Sistema::verificar()
+{
+	
+}
+
+int main() {
+
+	cout << "VAI FUNFA" << endl;
+
+	return 0;
+}
