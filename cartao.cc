a420d357c3d756ac5a85ee7b7b9cf0505abdef05
LEANDRO SANTOS GRASSI CARDOSO <11111917@l1815109.portoalegre.pucrsnet.br>
2015-11-12 20:45:44 -0200

    construtor cartão

--- cartao.cc
+++ cartao.cc
@@ -3,12 +3,36 @@
 #include "cartao.h"
 using namespace std;
 
-Cartao::Cartao (int i, int d, int m, int a)
+Cartao::Cartao (int ID, int dia, int mes, int ano)
 {
-	ID=i;
-	dia=d;
-	mes=m;
-	ano=a;
+	ID=ID;
+	dia=dia;
+	mes=mes;
+	ano=ano;
+};
+
+void Cartao::ler(){
+	int ID;
+	int Dia;
+	int Mes;
+	int Ano;
+	ifstream file;
+	file.open("cartao.txt", ios::in);
+	if (!file.is_open())
+    {
+        cout << "Erro fatal!" << endl;
+        return; // Aborta programa
+    }
+	file>>ID>>Dia>>Mes>>Ano;
+	cout<< ID<<endl;
+	cout<< Dia<<endl;	
+	cout<< Mes<<endl;
+	cout<< Ano<<endl;		
+	file.close();
+	};
+int main(){
+
+	Cartao b;
+
+	b.ler();
 }
-
-
