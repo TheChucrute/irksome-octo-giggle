d18a5e639f47449f96ec8a0487750035beb9bc26
OCTAVIO PITREZ CARPES <13202108@l1846617.portoalegre.pucrsnet.br>
2015-11-18 12:30:22 -0200

    PIOR GITHUB

--- cartao.cc
+++ cartao.cc
@@ -6,9 +6,33 @@ using namespace std;
 
 void Cartao::ler(){
 
-	int id;
+	int c1,id;
+
+	cout << "Insira o cartao: " << endl;
+	cin >> c1;
+
+	if(c1==1){	
+
 	ifstream file("cartao1.txt");
+	
+	cout << "estou no ler do cartao" << endl;
+
 	file >> id;
 
 	cout << id << endl;
+	};
+
+	
+	if(c1==2){	
+
+	ifstream file("cartao2.txt");
+	
+	cout << "estou no ler do cartao" << endl;
+
+	file >> id;
+
+	cout << id << endl;
+	};
+
+	
 };
